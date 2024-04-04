3f95d9f4ffacb3ec632d6ad325205e1b3f6070ee
tm6789 <tcvmarino@gmail.com>
2024-04-04 16:05:58 -0700

    PC Access in PokeNav
    Adds the ability to access the PC in the overworld through the PokeNav.

--- include/strings.h
+++ include/strings.h
@@ -2989,6 +2989,7 @@ extern const u8 gText_Cancel6[];
 extern const u8 gText_CheckMapOfHoenn[];
 extern const u8 gText_CheckPokemonInDetail[];
 extern const u8 gText_CallRegisteredTrainer[];
+extern const u8 gText_Pokenav_Access_PC[];
 extern const u8 gText_CheckObtainedRibbons[];
 extern const u8 gText_PutAwayPokenav[];
 extern const u8 gText_CheckPartyPokemonInDetail[];
@@ -3000,6 +3001,7 @@ extern const u8 gText_FindCutePokemon[];
 extern const u8 gText_FindSmartPokemon[];
 extern const u8 gText_FindToughPokemon[];
 extern const u8 gText_ReturnToConditionMenu[];
+extern const u8 gText_Pokenav_Cannot_Access_PC[];
 extern const u8 gText_NoRibbonWinners[];
 
 // PokéNav
