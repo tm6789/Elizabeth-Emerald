034ccb34d24573d7d613a198e9ce7ede432138be
tm6789 <tcvmarino@gmail.com>
2024-04-19 14:15:45 -0700

    Bag Sorting
    Allows player to sort bag by name, type, or quantity.

--- src/item.c
+++ src/item.c
@@ -28,7 +28,8 @@ EWRAM_DATA struct BagPocket gBagPockets[POCKETS_COUNT] = {0};
 #include "data/pokemon/item_effects.h"
 #include "data/items.h"
 
-static u16 GetBagItemQuantity(u16 *quantity)
+// code
+u16 GetBagItemQuantity(u16 *quantity)
 {
     return gSaveBlock2Ptr->encryptionKey ^ *quantity;
 }
